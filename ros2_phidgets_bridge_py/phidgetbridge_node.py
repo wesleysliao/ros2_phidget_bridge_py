f72fcb5bfafce497a6112107bdf37a57d4c23cf7
Wesley Liao <wes@wesliao.com>
2020-01-14 13:31:32 -0500

    Added Phidget22Python and added functionality from ROS1 version. Not tested.

--- ros2_phidgets_bridge_py/phidgetbridge_node.py
+++ ros2_phidgets_bridge_py/phidgetbridge_node.py
@@ -8,35 +8,85 @@ from geometry_msgs.msg import Vector3
 from std_msgs.msg import Header
 
 
+from Phidget22.Devices.VoltageRatioInput import VoltageRatioInput, BridgeGain
+
+
 class PhidgetBridgeNode(Node):
 
 
-    def __init__(self):
+    def __init__(self,
+                 topic,
+                 frame_id,
+
+                 bridge_channel,
+                 bridge_serial = None,
+                 bridge_hubport= 0,
+
+                 data_rate_Hz = 60,
+
+                 gain = BridgeGain.BRIDGE_GAIN_128,
+
+                 force_conversion_multiple = 1.0,
+                 force_conversion_offset = 0.0):
         super().__init__('phidgetbridge_node')
 
-        self.pub_force = self.create_publisher(WrenchStamped, 
-                                               'load_cell_force', 
-                                               1)
-        self.timer_force = self.create_timer(1, self.timer_callback)
+        self.topic = topic
+        self.frame_id = frame_id
+        
+        self.pb_ch = VoltageRatioInput()
+        self.pb_ch.setChannel(int(bridge_channel))
+        self.pb_ch.setHubPort(bridge_hubport)
+        self.pb_ch.setIsHubPortDevice(bridge_hubportdevice)
+        if bridge_serial is not None:
+            self.pb_ch.setDeviceSerialNumber(bridge_serial)
+        
+        self.data_rate_Hz = data_rate_Hz
+        self.data_interval_ms = int((1.0/self.data_rate_Hz)*1000)
+        
+        self.gain = gain
+
+        self.force_conversion_multiple = float(force_conversion_multiple)
+        self.force_conversion_offset = float(force_conversion_offset)
 
 
-    def timer_callback(self):
+        self.pub_force = self.create_publisher(WrenchStamped, self.topic, 1)
+
+        self.pb_ch.setOnAttachHandler(self.onAttachHandler)
+        self.pb_ch.setOnDetachHandler(self.onDetachHandler)
+        self.pb_ch.setOnVoltageRatioChangeHandler(self.onVoltageRatioChangeHandler)
+
+        self.pb_ch.openWaitForAttachment(1000)
+
+    def onAttatchHandler(self, channel):
+        channel.setDataInterval(self.data_interval_ms)
+        channel.setVoltageRatioChangeTrigger(0.0)
+        channel.setBridgeGain(self.gain)
+        channel.setBridgeEnabled(True)
+
+    def voltageRatioToForce(self, voltageRatio):
+        return ((voltage_ratio*self.force_conversion_multiple) 
+                 + self.force_conversion_offset)
+
+    def onVoltageRatioChangeHandler(self, channel, voltageRatio):
         self.pub_force.publish(
             WrenchStamped(
                 header=Header(
                     stamp=self.get_clock().now().to_msg(),
-                    frame_id="ball_knob_1"),
+                    frame_id=self.frame_id),
                 wrench=Wrench(
-                    force=Vector3(x=10.0, y=0.0, z=0.0),
+                    force=Vector3(x=self.voltageTatioToForce(voltageRatio),
+                                  y=0.0,
+                                  z=0.0),
                     torque=Vector3(x=0.0, y=0.0, z=0.0))))
-
 
 
 
 def main(args = None):
     rclpy.init(args=args)
 
-    pbnode = PhidgetBridgeNode()
+    pbnode = PhidgetBridgeNode(topic="load_cell_force",
+                               frame_id="load_cell",
+                               bridge_channel=0)
 
     rclpy.spin(pbnode)
 
